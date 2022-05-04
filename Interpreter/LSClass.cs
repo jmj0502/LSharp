79bd91843db2016930dac673febeb0b7ae688383
Martin Juarez <juarezmachadojorgemartin@gmail.com>
2022-05-04 08:31:07 -0400

    Improvements: A basic implementation of constructors was added into the language.

--- Interpreter/LSClass.cs
+++ Interpreter/LSClass.cs
@@ -6,13 +6,25 @@ using System.Threading.Tasks;
 
 namespace LSharp.Interpreter
 {
-    public class LSClass
+    public class LSClass : ICallable
     {
         public readonly string Name;
 
         public LSClass(string name)
         {
             Name = name;
+        }
+
+        public object Call(Interpreter interpreter,
+            List<object> arguments)
+        {
+            var instance = new LSInstance(this);
+            return instance;
+        }
+
+        public int Arity()
+        {
+            return 0;
         }
 
         public override string ToString()
---
+++ Interpreter/LSInstance.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSharp.Interpreter
+{
+    public class LSInstance
+    {
+        private LSClass lsClass;
+
+        public LSInstance(LSClass lsClass)
+        {
+            this.lsClass = lsClass;
+        }
+
+        public override string ToString()
+        {
+            return $"{lsClass} instance";
+        }
+    }
+}
