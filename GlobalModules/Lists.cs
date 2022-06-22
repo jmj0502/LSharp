4446e96a7236ebceb88d38d70957340a73c9e887
Martin Juarez <juarezmachadojorgemartin@gmail.com>
2022-06-22 09:34:55 -0400

    Adding the concat method to the list module.

--- GlobalModules/Lists.cs
+++ GlobalModules/Lists.cs
@@ -18,6 +18,9 @@ namespace LSharp.GlobalModules
             moduleBody.Define("indexOf", new IndexOf("list"));
             moduleBody.Define("join", new Join());
             moduleBody.Define("add", new Add());
+            moduleBody.Define("prepend", new Prepend());
+            moduleBody.Define("insert", new Insert());
+            moduleBody.Define("concat", new Concat());
             return moduleBody;
         }
     }
@@ -104,6 +107,26 @@ namespace LSharp.GlobalModules
         public override string ToString()
         {
             return "<native function list.insert>";
+        }
+    }
+
+    public class Concat : ICallable
+    {
+        public int Arity()
+        {
+            return 2;
+        }
+
+        public object Call(Interpreter.Interpreter interpreter, List<object> arguments)
+        {
+            var lhsList = (List<object>)arguments[0];
+            var rhsList = (List<object>)arguments[1];
+            return lhsList.Concat(rhsList).ToList();
+        }
+
+        public override string ToString()
+        {
+            return "<native function list.concat>";
         }
     }
 }
