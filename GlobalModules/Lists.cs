15666b02d0ac30fbd7a0bc67dcde8c6299221516
Martin Juarez <juarezmachadojorgemartin@gmail.com>
2022-06-22 09:07:28 -0400

    Improvements: Three new native functions were added to the List method.

--- GlobalModules/Lists.cs
+++ GlobalModules/Lists.cs
@@ -16,7 +16,94 @@ namespace LSharp.GlobalModules
             moduleBody.Define("slice", new Slice("list"));
             moduleBody.Define("contains", new Contains("list"));
             moduleBody.Define("indexOf", new IndexOf("list"));
+            moduleBody.Define("join", new Join());
+            moduleBody.Define("add", new Add());
             return moduleBody;
+        }
+    }
+
+    public class Join : ICallable
+    {
+        public int Arity()
+        {
+            return 2;
+        }
+
+        public object Call(Interpreter.Interpreter interpreter, List<object> arguments)
+        {
+            var list = (List<object>)arguments[0];
+            var separatorStr = (string)arguments[1];
+            return string.Join(separatorStr, list);
+        }
+
+        public override string ToString()
+        {
+            return "<native function list.join>";
+        }
+    }
+
+    public class Add : ICallable
+    {
+        public int Arity()
+        {
+            return 2;
+        }
+
+        public object Call(Interpreter.Interpreter interpreter, List<object> arguments)
+        {
+            var list = (List<object>)arguments[0];
+            var newItem = arguments[1];
+            list.Add(newItem);
+            return (double)list.Count;
+        }
+
+        public override string ToString()
+        {
+            return "<native function list.add>";
+        }
+    }
+
+    public class Prepend : ICallable
+    {
+        public int Arity()
+        {
+            return 2;
+        }
+
+        public object Call(Interpreter.Interpreter interpreter, List<object> arguments)
+        {
+            var list = (List<object>)arguments[0];
+            var newItem = arguments[1];
+            list.Insert(0, newItem);
+            return (double)list.Count;
+        }
+
+        public override string ToString()
+        {
+            return "<native function list.prepend>";
+        }
+    }
+
+    public class Insert : ICallable
+    {
+        public int Arity()
+        {
+            return 3;
+        }
+
+        public object Call(Interpreter.Interpreter interpreter, List<object> arguments)
+        {
+            var list = (List<object>)arguments[0];
+            var indexValue = (double)arguments[1];
+            var index = (int)indexValue;
+            var newItem = arguments[2];
+            list.Insert(index, newItem);
+            return (double)list.Count;
+        }
+
+        public override string ToString()
+        {
+            return "<native function list.insert>";
         }
     }
 }
