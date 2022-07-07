922d6740cc8aac818acb1a65e1cd3543a9724530
Martin Juarez <juarezmachadojorgemartin@gmail.com>
2022-07-07 09:08:05 -0400

    Improvements: The dictionary module is a work in progress.

---
+++ GlobalModules/Dictionaries.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSharp.GlobalModules
+{
+    class Dictionaries : IModule
+    {
+        public Enviroment.Enviroment GenerateBody()
+        {
+            var moduleBody = new Enviroment.Enviroment();
+            return moduleBody;
+        }
+    }
+}
