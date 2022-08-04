4249cbeb5389d90cb9b0b288ee02d435afb73518
Martin Juarez <juarezmachadojorgemartin@gmail.com>
2022-08-04 09:59:00 -0400

    Improvements: LSharp JSON parser is a work in progress.

---
+++ Scanner/JSONScanner.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LSharp.Tokens;
+
+namespace LSharp.Scanner
+{
+    class JSONScanner
+    {
+        private readonly string source;
+        private readonly List<Token> tokens;
+        private int start;
+        private int current;
+        private int line = 1;
+
+        public JSONScanner(string source)
+        {
+            this.source = source;
+        }
+
+        private void scanToken()
+        {
+            var c = advance();
+            switch (c)
+            {
+                case '"': addToken(TokenType.STRING); break; 
+            }
+        }
+
+        private void addToken(TokenType token) { }
+
+        public char advance()
+        {
+            return source[current++];
+        }
+    }
+}
